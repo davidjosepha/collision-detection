3ba4ebcc26d71943d7f34e9617cf9ce50b93c063
David Anderson <davidjosepha@gmail.com>
2015-04-16 19:11:00 -0500

    Add empty main file

--- main.cpp
+++ main.cpp
@@ -1,44 +1,11 @@
 #include <cstdlib>
-#include <cstdio>
-#include <cstdarg>
-#include <cmath>
-#include <iostream>
-#include <random>
-#include <chrono>
 #define GL_GLEXT_PROTOTYPES
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
 #include <GL/glut.h>
 #endif
-#include "geom_3d.h"
-#include "cuboid.h"
-#include "controller.h"
-#include "viewer.h"
 
 int main(int argc, char * argv[]) {
-  //float color[3] = {0.0, 1.0, 0.0};
-  Vec center = Vec(0.0, 0.0, 0.0);
-  Vec velocity = Vec(0.0, 0.0, 0.0);
-  Vec axis = Vec(0.0, 1.0, 0.0);
-
-  float angle = 0.0;
-  float rotation = 0.0;
-  Cuboid cuboid = Cuboid(1.0,1.0,1.0, center, 1.0, axis, velocity, angle, rotation);
-
-  //Vec impact_position = Vec(1.0, 1.0, 0.0);
-  //Vec impact_velocity = Vec(0.003, -0.003, 0.0);
-  Vec impact_position = Vec(0.5, 0.5, 1.0);
-  Vec impact_center = Vec(0.0, 0.0, 3.0);
-  Vec impact_velocity = Vec(0.0, 0.0, -0.005);
-
-  cuboid.impact(impact_position, 12.0,
-                impact_center, 2.0,
-                impact_velocity, Vec(0.0, 0.0, -1.0).Normalized()); 
-
-  Controller controller = Controller(cuboid);
-  Viewer viewer = Viewer(cuboid, controller);
-  viewer.initGlut(argc, argv);
-
   return 0;
 }
