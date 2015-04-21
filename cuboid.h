7aa008e39f9818f2aedb385a5c4ebf7813844be3
David Anderson <davidjosepha@gmail.com>
2015-04-20 19:42:02 -0500

    Make the triangle mesh actually make sense

--- cuboid.h
+++ cuboid.h
@@ -17,10 +17,9 @@ class Cuboid : public Object {
     const static int NUM_TRIS = 12;
     glm::vec3 verts_[NUM_VERTS];
     glm::highp_ivec3 tris_[NUM_TRIS] = {
-      { 0, 3, 1 },  { 0, 1, 5 },  { 0, 5, 3 },
-      { 2, 1, 3 },  { 2, 7, 1 },  { 2, 3, 7 },
-      { 4, 5, 1 },  { 4, 1, 7 },  { 4, 7, 5 },
-      { 6, 3, 5 },  { 6, 7, 3 },  { 6, 5, 7 }
+      { 0, 1, 3 },  { 0, 3, 2 },  { 4, 0, 2},  { 4, 2, 6},
+      { 5, 4, 6 },  { 5, 6, 7 },  { 1, 5, 7},  { 1, 7, 3},
+      { 0, 4, 5 },  { 0, 5, 1 },  { 2, 3, 7},  { 2, 7, 6}
     };
 
     void genverts(const float & x, const float & y, const float & z);
