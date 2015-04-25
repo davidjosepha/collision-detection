fd6ee7d46cc1f6e6111ddcafbde51039ab6ef5c9
David Anderson <davidjosepha@gmail.com>
2015-04-25 11:45:42 -0500

    Fix triangle array

--- cuboid.h
+++ cuboid.h
@@ -19,7 +19,7 @@ class Cuboid : public Object {
     glm::highp_uvec3 tris_[NUM_TRIS] = {
       { 0, 3, 1 },  { 0, 2, 3 },  { 4, 2, 0 },  { 4, 6, 2 },
       { 5, 6, 4 },  { 5, 7, 6 },  { 1, 7, 5 },  { 1, 3, 7 },
-      { 0, 5, 4 },  { 0, 1, 5 },  { 2, 7, 2 },  { 2, 6, 7 }
+      { 0, 5, 4 },  { 0, 1, 5 },  { 2, 3, 7 },  { 2, 6, 7 }
     };
 
     void genverts(const float & x, const float & y, const float & z);
