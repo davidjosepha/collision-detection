9dd106c55af6d6ba329daa428944c9257c0c763b
David Anderson <davidjosepha@gmail.com>
2015-05-15 09:20:32 -0500

    Add loop to generate moment of inertia spheres

--- cuboid.cpp
+++ cuboid.cpp
@@ -33,18 +33,27 @@ void Cuboid::genchunks(float x, float y, float z) {
         chunks_[CUBES_PER_SIDE * CUBES_PER_SIDE * i + CUBES_PER_SIDE * j + k]
             = { x * (i + .5) / CUBES_PER_SIDE,
                 y * (j + .5) / CUBES_PER_SIDE,
-                z * (k + .5) / CUBES_PER_SIDE,
-                0.0 };
+                z * (k + .5) / CUBES_PER_SIDE };
       }
     }
   }
 }
 
 void Cuboid::gensphere() {
+  for (int i = 0; i < CUBES_PER_SIDE * CUBES_PER_SIDE * CUBES_PER_SIDE; i++) {
+    sphere_[i] = inertia_at_axis(sphere_verts_[i]);
+  }
 }
 
-void Cuboid::inertia_at_axis(glm::vec3 const & axis) {
-  glm::vec3 orient_axis = glm::cross(axis, glm::vec3(0.0, 1.0, 0.0));
-  float orient_angle = glm::angle(axis, glm::vec3(0.0, 1.0, 0.0));
-  glm::mat4 orient_mat = glm::rotate(orient_angle, orient_axis);
+float Cuboid::inertia_at_axis(glm::vec3 const & axis) {
+  //glm::vec3 orient_axis = glm::cross(axis, glm::vec3(0.0, 0.0, 1.0));
+  //float orient_angle = glm::angle(axis, glm::vec3(0.0, 0.0, 1.0));
+  //glm::mat4 orient_mat = glm::rotate(orient_angle, orient_axis);
+  
+  // apply matrix to point
+  //
+  //// sum over (x^2 + y^2) * m
+  // v = axis
+  // x = point
+  // || (x - (v_hat dot x) * v_hat ||^2
 }
